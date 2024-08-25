1ec8904f6309666fba16b3ebdc45c780aaed1f3f
EXuanPang <e.xuan@hotmail.com>
2024-08-25 21:32:51 +0900

    feat(ex):Completed Exercises

--- ex-7.js
+++ ex-7.js
@@ -1,5 +1,6 @@
 //Start coding here
-const greeting = "Hello ! 😁";
-
-console.log(greeting)
+const greeting = function(){
+    console.log("Hello ! 😁");
+}
+greeting();
 console.log("This line will be executed without any error.");
